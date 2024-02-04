2b8147053e93644d4cdbc54f1ae1d4d88d1b2156
achiyaAmrosi <ahia.amrosi@mail.huji.ac.il>
2024-02-04 16:20:27 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pySpectrum',
+    version='0.1',
+    packages=['pyspectrum', 'pyspectrum.spectrum', 'pyspectrum.calibration', 'pyspectrum.gamma_find_peak',
+              'pyspectrum.activity_analysis'],
+    url='',
+    license='MIT license',
+    author='Achiya Yosef Amrusi',
+    author_email='ahia.amrosi@mail.huji.ac.il',
+    description='package for spectrum handling'
+)
