e5fdd6ef2083300d5d9e0bd5bab9c1c807515e0a
achiyaAmrosi <ahia.amrosi@mail.huji.ac.il>
2024-10-05 22:15:40 +0300

    setup updated

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='pySpectrum',
     version='1.0',
-    packages=['pyspectrum', 'pyspectrum.spectrum', 'pyspectrum.calibration', 'pyspectrum.peak_identification',
-              'pyspectrum.activity_analysis'],
-    url='',
+    packages=['pyspectrum', 'pyspectrum.peak', 'pyspectrum.spectrum', 'pyspectrum.calibration',
+              'pyspectrum.peak_fitting', 'pyspectrum.detector_parser', 'pyspectrum.peak_identification'],
+    url='https://github.com/achiyaAmrusi/pySpectrum',
     license='MIT license',
     author='Achiya Yosef Amrusi',
     author_email='ahia.amrosi@mail.huji.ac.il',
-    description='package for spectrum handling'
+    description='spectrum and peak analysis tools'
 )
